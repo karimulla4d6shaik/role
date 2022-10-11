a74a36769af2a1691db62d3cedd35fb4674e25c8
1992k <1992k@DESKTOP-65DMD5F>
2022-10-12 00:02:19 +0530

    db properties and security changes

--- role-app/src/main/java/com/role/app/config/SecurityConfig.java
+++ role-app/src/main/java/com/role/app/config/SecurityConfig.java
@@ -1,24 +1,27 @@
 package com.role.app.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@SuppressWarnings("deprecation")
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+public class SecurityConfig {
 	@Autowired
 	private OncePerRequestFilterImpl oncePerRequestFilterImpl;
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
 		.authorizeRequests()
 		.anyRequest()
 		.authenticated()
 		.and()
 		.addFilterBefore(oncePerRequestFilterImpl, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
 	}
 }
